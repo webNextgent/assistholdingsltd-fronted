a02952ee30e24240e6b6574dbabf0bb4eeda55ec
Kajal <fhkajal.ouritspace@gmail.com>
2026-02-23 11:53:06 +0600

    fix

--- src/components/shared/Footer/Footer.tsx
+++ src/components/shared/Footer/Footer.tsx
@@ -84,7 +84,7 @@ const Footer: React.FC = () => {
             © 2025 Assist Holdings Limited | All Rights Reserved.
           </p>
           <p className="mb-4 md:mb-0 text-gray-300">
-           Development, SEO, & Ads By{" "}
+           Development By{" "}
             <a className="font-bold " href="https://nextgent.org">NextGent.org </a>
           </p>
 
