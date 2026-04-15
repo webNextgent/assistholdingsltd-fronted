24934afba4e055a48b16d923765c8910226f7816
Nextgent Org <web.nextgent@gmail.com>
2026-04-15 15:26:51 +0600

    update

--- src/app/(withCommonLayout)/blogs/page.tsx
+++ src/app/(withCommonLayout)/blogs/page.tsx
@@ -10,7 +10,7 @@ const Blogs = () => {
       <LandownerBanner
         img={
           // "https://t3.ftcdn.net/jpg/04/96/48/04/360_F_496480434_VtvDbJ3I7u3UHi2pkQuaTahV2TegLfCd.jpg"
-          "https://i.postimg.cc/HWF8xYB6/thatselby-Ler7uco-R7vc-unsplash.jpg"
+          "https://i.postimg.cc/MGHkMSGp/pexels-felix-haumann-1938529-10143241.jpg"
         }
         title="blogs"
         // text="find your nest"
